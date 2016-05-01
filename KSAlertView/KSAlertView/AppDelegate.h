c05b8204e575e4992d7b784b70387850587c3cc2
沈力凯 <Kelly@kellydemacbook-pro.local>
2016-05-02 00:25:41 +0800

    初始化项目

---
+++ KSAlertView/KSAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSAlertView
+//
+//  Created by 沈力凯 on 16/5/2.
+//  Copyright © 2016年 Kelly Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSAlertView/KSAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSAlertView
+//
+//  Created by 沈力凯 on 16/5/2.
+//  Copyright © 2016年 Kelly Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
